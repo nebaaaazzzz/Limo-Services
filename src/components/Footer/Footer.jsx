9162f98311f4b6722accc7b4b3f632f974d226e0
Yosef781227 <yosefnakachew101@gmail.com>
2023-04-04 14:02:54 +0300

    new file

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -43,7 +43,7 @@ const Footer = () => {
                 <Link to="/home" className=" d-flex align-items-center gap-2">
                   <i class="ri-car-line"></i>
                   <span>
-                    Rent Car <br /> Service
+                  Limousine<br />Service
                   </span>
                 </Link>
               </h1>
@@ -72,12 +72,11 @@ const Footer = () => {
           <Col lg="3" md="4" sm="6">
             <div className="mb-4">
               <h5 className="footer__link-title mb-4">Head Office</h5>
-              <p className="office__info">123 Zindabazar, Sylhet, Bangladesh</p>
-              <p className="office__info">Phone: +0995345875365</p>
-
-              <p className="office__info">Email: muhib5532@gmail.com</p>
-
-              <p className="office__info">Office Time: 10am - 7pm</p>
+             
+              <p className="office__info">Area the company works </p>    { /*123 Zindabazar, Sylhet, Bangladesh*/ }
+              <p className="office__info">Phone:</p>
+              <p className="office__info">Email:</p>
+              <p className="office__info">Office Time:</p>
             </div>
           </Col>
 
@@ -97,8 +96,8 @@ const Footer = () => {
           <Col lg="12">
             <div className="footer__bottom">
               <p className="section__description d-flex align-items-center justify-content-center gap-1 pt-4">
-                <i class="ri-copyright-line"></i>Copyright {year}, Developed by
-                Muhibur Rahman. All rights reserved.
+                <i class="ri-copyright-line"></i>Copyright {year}, Developed by Fejlesztő technology
+                . All rights reserved.
               </p>
             </div>
           </Col>
