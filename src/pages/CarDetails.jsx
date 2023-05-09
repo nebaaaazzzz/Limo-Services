16c73645ec5da6c11064346d9d4d6afa47073170
Yosef781227 <yosefnakachew101@gmail.com>
2023-05-09 13:00:51 +0300

    css of the card

--- src/pages/CarDetails.jsx
+++ src/pages/CarDetails.jsx
@@ -5,7 +5,7 @@ import { Container, Row, Col } from "reactstrap";
 import Helmet from "../components/Helmet/Helmet";
 import { useParams } from "react-router-dom";
 import BookingForm from "../components/UI/BookingForm";
-import PaymentMethod from "../components/UI/PaymentMethod";
+
 
 const CarDetails = () => {
   const { slug } = useParams();
