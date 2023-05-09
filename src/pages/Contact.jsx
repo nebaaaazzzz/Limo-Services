51db8985ec5932091737ad3ff4a87f24c61c90c4
Yosef781227 <yosefnakachew101@gmail.com>
2023-05-09 14:57:15 +0300

    css

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -122,12 +122,12 @@ const Contact = () => {
                 </p>
                 <div className=" d-flex align-items-center gap-2">
                   <h6 className="fs-6 mb-0">Phone:</h6>
-                  <p className="section__description mb-0">+88683896366</p>
+                  <p className="section__description mb-0">+8868389636</p>
                 </div>
 
                 <div className=" d-flex align-items-center gap-2">
                   <h6 className="mb-0 fs-6">Email:</h6>
-                  <p className="section__description mb-0">example@gmail.com</p>
+                  <p className="section__description mb-0">examp@gmail.com</p>
                 </div>
 
                 <h6 className="fw-bold mt-4">Follow Us</h6>
