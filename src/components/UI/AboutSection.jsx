2c796ba50d102e4b5f5568f4d95c88830148d8a9
bruhtesfa-gh <bruhtesfazelealem436@gmail.com>
2023-09-13 09:43:04 +0300

    Discover

--- src/components/UI/AboutSection.jsx
+++ src/components/UI/AboutSection.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
 import "../../styles/about-section.css";
-import aboutImg from "../../assets/all-images/cars-img/bmw-offer.png";
+import aboutImg from "../../assets/all-images/chrysler300s.png";
 
 const AboutSection = ({ aboutClass }) => {
   return (
